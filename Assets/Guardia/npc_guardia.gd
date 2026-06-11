34f8375e15d35d022fa74b591d90ee1b4a504d5c
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-06-10 22:02:36 -0300

    movimiento quieto cuando hay dialogo

--- Assets/Guardia/npc_guardia.gd
+++ Assets/Guardia/npc_guardia.gd
@@ -2,22 +2,33 @@ extends StaticBody2D
 
 @export var dialogue_resource: DialogueResource
 
+var cam
+var jugador
+
 func _ready():
 	$AreaInteraccion.interactuado.connect(_on_interactuado)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 func _on_interactuado():
-	var cam = get_tree().get_first_node_in_group("player").get_node("Camera2D2")
+	jugador = get_tree().get_first_node_in_group("player")
+	print("Inicio")
+	cam = get_tree().get_first_node_in_group("player").get_node("Camera2D2")
+
+	jugador.bloquear_movimiento(true)
+
 
 	var tween = create_tween()
 	tween.tween_property(cam, "offset:y", 50, 0.5)
 
 	DialogueManager.show_dialogue_balloon(dialogue_resource)
-
-	await get_tree().create_timer(3.0).timeout
-
-	var tween2 = create_tween()
-	tween2.tween_property(cam, "offset:y", 0, 0.5)
+	print("Fin")
 	
-func _on_area_interaccion_interactuado():
-	DialogueManager.show_dialogue_balloon(dialogue_resource)
+func _on_dialogue_ended(_resource):
+	if cam:
+		var tween = create_tween()
+		tween.tween_property(cam, "offset:y", 0, 0.5)
+		
+	if jugador:
+		jugador.bloquear_movimiento(false)
+
 	
