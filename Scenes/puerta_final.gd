7fc099d33dbc4a7c5d17e6d474cf44991e0fff3e
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-06-10 22:29:19 -0300

    puerta final

--- Scenes/puerta_final.gd
+++ Scenes/puerta_final.gd
@@ -4,12 +4,36 @@ var abierta = false
 @onready var collision = $CollisionShape2D
 @onready var luz = $PointLight2D
 
+@export var dialogue_resource: DialogueResource
+
+var jugador
+
+func _ready():
+	$AreaInteraccion.interactuado.connect(_on_interactuado)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+func _on_interactuado():
+	jugador = get_tree().get_first_node_in_group("player")
+	if abierta:
+		return
+
+	jugador.bloquear_movimiento(true)
+	
+	DialogueManager.show_dialogue_balloon(dialogue_resource)
+	
+func _on_dialogue_ended(_resource):
+	if jugador:
+		jugador.bloquear_movimiento(false)
+
 func toggle_puerta():
 	abierta = !abierta
 	$CollisionShape2D.disabled = abierta
 	if abierta:
 		$Sprite2D.hide()
 		luz.abrir()
+		# Desactivar interacción
+		$AreaInteraccion.monitoring = false
+		
 	else:
 		$Sprite2D.show()
 		luz.cerrar()
