c5f2e4f9c54c35d88966e0b4e28496419fcb0df5
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-06-23 17:45:53 -0300

    Cinematicas Inmortal y Seguridad

---
+++ Scenes/Cinematicas/Inmortal/guardia_de_seguridad.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+
+func play_idle():
+	play("IdleGuardia")
+	
+func play_Correr():
+	play("RunGuardia")
+	
+func play_Pistola():
+	play("ArmaGuardia")
+	
+func play_Disparo():
+	play ("ShotGuardia")
+	
