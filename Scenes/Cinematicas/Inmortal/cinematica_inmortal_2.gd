82e9fb3f68b3f765ca59c30cd8e68c917ade3b61
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-06-24 12:24:53 -0300

    Cinematicas Finalizadas (falta agregar el dialogo)

--- Scenes/Cinematicas/Inmortal/cinematica_inmortal_2.gd
+++ Scenes/Cinematicas/Inmortal/cinematica_inmortal_2.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 @onready var light = $Sprite2D/LuzRoja
+@onready var dialogue = load("res://Dialogue/dialogo_central.dialogue")
 
 @export var rotation_speed := 5.0
 @export var base_energy := 2.0
 @export var pulse_strength := 1.0
+
 
 func _ready():
 	light.color = Color.RED
