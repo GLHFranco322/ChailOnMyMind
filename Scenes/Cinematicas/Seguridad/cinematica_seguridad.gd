f8170f66db4108766eb29207a5935cee1f8d0074
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-08-10 20:34:15 -0300

    omitir escenas

--- Scenes/Cinematicas/Seguridad/cinematica_seguridad.gd
+++ Scenes/Cinematicas/Seguridad/cinematica_seguridad.gd
@@ -9,6 +9,13 @@ extends Node2D
 func _ready():
 	light.color = Color.RED
 
+	$CanvasLayer/OmitirButton.modulate.a = 0.0
+	
+	await get_tree().create_timer(5.0).timeout
+	
+	var tween = create_tween()
+	tween.tween_property($CanvasLayer/OmitirButton,"modulate:a",1.0,1.5)
+
 func _process(delta):
 	# Giro de la sirena
 	light.rotation += rotation_speed * delta
@@ -20,3 +27,6 @@ func _process(delta):
 func _on_animation_player_animation_finished(anim_name: StringName):
 	if anim_name == "CinematicaSeguridad":
 		get_tree().change_scene_to_file("res://Scenes/Cinematicas/Inmortal/cinematica_inmortal2.tscn")
+
+func _on_omitir_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/comisaria.tscn")
